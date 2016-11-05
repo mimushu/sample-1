647892ac48df74359af39d3f2bf274c3452ccdb6
三村周 <shu-P@mimuraamane-no-MacBook-Pro-2.local>
2016-11-05 12:14:42 +0900

    もどした

--- sample-2/ChangeLabelViewController.swift
+++ sample-2/ChangeLabelViewController.swift
@@ -36,10 +36,5 @@ class ChangeLabelViewController: UIViewController {
     }
     */
 
-    @IBAction func changeButtonPushed(_ sender: Any) {
-        print("Hello! Button is pushed.")
-        sampleLabel.text="長澤まさみだいすき"
-        print("aaa")
-        sampleButton.setTitle("new message", for: UIControlState.normal)
-    }
+   
 }
